1daef3f569632412aa3c5e9246d2ee1f28bb58c7
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-01-13 21:27:47 -0500

    Create blog card component

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,23 +1,15 @@
 import { NextPage } from "next";
+import BlogCard from "../components/BlogCard";
 
 interface Props {}
 
 const Blogs: NextPage<Props> = () => {
   return (
-    <div className="bg-green-100 max-w-3xl mx-auto p-5 rounded">
-      <div className="bg-green-100 p-2">
-        <h1 className="text-3xl text-gray-900 font-semibold">
-          Lorem ipsum dolor sit amet.
-        </h1>
-        <p className="text-gray-500">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias,
-          recusandae magni accusantium assumenda iure suscipit animi voluptate,
-          ipsa nemo officiis nihil iusto nisi explicabo consequuntur ex ratione
-          officia sit inventore repellendus illo? Nemo, inventore aliquid? Harum
-          quo illum iste, doloribus maiores architecto nobis asperiores eum
-          excepturi eius porro esse consequatur.
-        </p>
-      </div>
+    <div className="max-w-3xl mx-auto p-5 space-y-5">
+      <BlogCard title="This is my blog" desc="This is my desc"/>
+      <BlogCard title="This is my blog" desc="This is my desc"/>
+      <BlogCard title="This is my blog" desc="This is my desc"/>
+      <BlogCard title="This is my blog" desc="This is my desc"/>
     </div>
   );
 };
