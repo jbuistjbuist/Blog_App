cad95e97be88e24299f19a0423af2085f1d31450
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-01-14 19:59:31 -0500

    Refactor blogs to use getStaticProps

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,28 +1,27 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import { useEffect, useState } from "react";
 import BlogCard from "../components/BlogCard";
 
 interface Props {}
 
-const Blogs: NextPage<Props> = () => {
-  const [posts, setPosts] = useState<
-    { title: string; slug: string; meta: string }[]
-  >([]);
-
-  const fetchPosts = async () => {
-    const res = await fetch ('/api/posts').then(data => data.json())
-    setPosts(res.postInfo);
-  }
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
+const Blogs: NextPage<Props> = ({ posts }) => {
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
-      { posts.map(post => <BlogCard title={post.title} desc={post.meta}/>)}
+      {posts.map((post) => (
+        <BlogCard title={post.title} desc={post.meta} />
+      ))}
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("http://localhost:3000/api/posts").then((data) =>
+    data.json()
+  );
+
+  return {
+    props: { posts: res.postInfo },
+  };
 };
 
 export default Blogs;
