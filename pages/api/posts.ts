c24f1329e78b496d986777e9e514bb63a1ac683d
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-01-14 19:15:41 -0500

    Add more prepopulated posts with meta information

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,14 +1,31 @@
 import { NextApiHandler } from "next";
+import matter from "gray-matter";
+import fs from "fs";
+import path from "path";
 
 const handler: NextApiHandler = (req, res) => {
   const { method } = req;
 
   switch (method) {
-    case "GET":
-      return res.json({ ok: true });
+    case "GET": {
+      const data = readPostsInfo();
+      return res.json({ postInfo: data });
+    }
     default:
       return res.status(404).send("Not found");
   }
+};
+
+const readPostsInfo = () => {
+  const dirPathToRead = path.join(process.cwd(), "posts");
+  const dirs = fs.readdirSync(dirPathToRead);
+  dirs.map(filename => {
+    const filePathToRead = path.join(process.cwd(), "posts/" + filename);
+    const fileContent = fs.readFileSync(filePathToRead, {encoding: 'utf-8'});
+    console.log(matter(fileContent))
+  })
+
+  return ""
 };
 
 export default handler;
