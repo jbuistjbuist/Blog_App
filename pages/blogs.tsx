049028f16c12cfd74211c3e2e61132ec73615c7c
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-01-21 19:32:52 -0500

    Add unique links with slugs to each blog post listed

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -16,7 +16,12 @@ const Blogs: NextPage<Props> = ({ posts }) => {
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
       {posts.map((post) => (
-        <BlogCard title={post.title} desc={post.meta} />
+        <BlogCard
+          key={post.slug}
+          title={post.title}
+          desc={post.meta}
+          slug={post.slug}
+        />
       ))}
     </div>
   );
